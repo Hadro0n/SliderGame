8c23173242c1310834b66f9731cf40a1be4b5097
Hadro0n <v.w.yakunin@gmail.com>
2025-03-27 15:00:34 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Владислав Якунин on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
